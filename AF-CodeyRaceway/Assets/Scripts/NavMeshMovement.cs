30ce92d7b45f037be60fa0edc9df383cc7cdbb69
Alexander <alexanderfomenko2011@gmail.com>
2023-01-28 15:02:38 -0500

    Raceway Step182
    almost done :anguished:

--- AF-CodeyRaceway/Assets/Scripts/NavMeshMovement.cs
+++ AF-CodeyRaceway/Assets/Scripts/NavMeshMovement.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshMovement : MonoBehaviour
 {
+    public Transform goal;
+    private NavMeshAgent agent;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        goal = GameObject.FindGameObjectWithTag("Obstacle").transform;
+        agent = GetComponent<NavMeshAgent>();   
+        agent.destination = goal.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+        }
     }
 }
