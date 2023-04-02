7e69ba3b608b90609bf0bbf04fc7def2a31b4e12
Alexander <alexanderfomenko2011@gmail.com>
2023-04-02 13:57:53 -0400

    Step40
    Trying to make the diamond move.

--- AF-Sulky Slimes/Assets/Scripts/Collectable.cs
+++ AF-Sulky Slimes/Assets/Scripts/Collectable.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    public float distanceToMove;
+    public float distanceToMove = 50;
 
     private Vector3 startingPosition;
     private Vector3 endingPosition;
@@ -16,21 +16,22 @@ public class Collectable : MonoBehaviour
     void Start()
     {
         startingPosition = transform.position;
-        //endingPosition = new Vector3(48.33f, 60+distanceToMove, 58);
-        endingPosition = new Vector3(0, distanceToMove, 0) + startingPosition;
+        endingPosition = startingPosition + new Vector3(distanceToMove, 0, 0);
+        //endingPosition = new Vector3(0, distanceToMove, 0) + startingPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(transform.position.y > 110)
+        if (transform.position.x <= startingPosition.x)
         {
             direction = 1;
         }
-        else if (transform.position.y < 60)
+        else if (transform.position.x >= endingPosition.x)
         {
             direction = -1;
         }
+
+        startingPosition.x += speed * direction * Time.deltaTime;
     }
 }
